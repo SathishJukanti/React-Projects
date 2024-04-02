374f2e6f9bda3256547c83481459b449ec73cdd5
Sathish Jukanti <32911989+SathishJukanti@users.noreply.github.com>
2024-04-02 09:19:38 +0530

    Delete 05ReactRouter directory

--- 05ReactRouter/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import About from "./About/About";
-import Contact from "./Contact/Contact";
-import Footer from "./Footer/Footer";
-import Header from "./Header/Header";
-import Home from "./Home/Home";
-import Github from "./Github/Github";
-import User from "./User/User";
-
-export { About, Contact, Footer, Header, Home, Github, User };
