0d3fb193cc8007c324799da3f7b083930e5dafe5
Sathish Jukanti <sathishjukanti@gmail.com>
2024-04-06 18:04:54 +0530

    React Router Dom

---
+++ 05ReactRouter/src/components/index.js
@@ -0,0 +1,10 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Github from "./Github/Github";
+import User from "./User/User";
+
+
+export {Footer, Header, Home, About, Contact, Github, User}
